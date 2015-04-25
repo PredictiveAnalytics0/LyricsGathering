b5e5ee0e28d1000d75b982cc73efb076ba794238
sangminLee <haveagt@naver.com>
2015-04-25 16:08:54 +0900

    just test unnecessary stuffs

---
+++ LyricsGathering/test/kr/ac/korea/dmqm/jenTest/SearchGenreOfIntendedSongs.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kr.ac.korea.dmqm.jenTest;
+
+
+/**
+ *
+ * @author plamere
+ */
+public class SearchGenreOfIntendedSongs {
+//	public static void main(String[] args) {
+//		EchoNestAPI en = new EchoNestAPI(JenAPIKey.JEN_API_KEY);
+//			try {
+//			en.search
+//			Params pa = new Params();
+//			pa.set("TITLE", "Daylight");
+//			List<Song> songs = en.searchSongs(pa);
+//			if(!songs.isEmpty()) {
+//				Song song = songs.get(0);
+//				int mode = song.getMode();
+//				System.out.println(mode);
+			
+//			List<Artist> cans = en.searchArtists("½ÎÀÌ");
+//			int size = cans.size();
+//			for(int i=0; i<size;i++) {
+//				Artist artist = cans.get(i);
+//				List<Song> songs = artist.getSongs();
+//				Iterator<Song> itt = songs.iterator();
+//				
+//				List<String> dupCheck = new ArrayList<String>();
+//				
+//				while(itt.hasNext()) {
+//					Song next = itt.next();
+//					if(dupCheck.contains(next.getTitle())) {
+//						continue;
+//					} else {
+//						dupCheck.add(next.getTitle());
+//					}
+//					System.out.println(next.getArtistName() + " ,  " + next.getTitle());
+//				}
+//			}
+			
+			
+//			System.out.println("total count : " + list.size());
+//			Iterator<String> it = list.iterator();
+//			while(it.hasNext()) {
+//				String genre = it.next();
+//				
+//				System.out.println(genre);
+//			}
+//		} catch (EchoNestException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+}
+
---
+++ LyricsGathering/test/kr/ac/korea/dmqm/jenTest/TopHottestSongs.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kr.ac.korea.dmqm.jenTest;
+
+import java.util.List;
+
+import kr.ac.korea.dmqm.util.JenAPIKey;
+
+import com.echonest.api.v4.EchoNestAPI;
+import com.echonest.api.v4.EchoNestException;
+import com.echonest.api.v4.Song;
+import com.echonest.api.v4.SongParams;
+
+/**
+ *
+ * @author plamere
+ */
+public class TopHottestSongs {
+
+	public static void main(String[] args) throws EchoNestException {
+		EchoNestAPI en = new EchoNestAPI(JenAPIKey.JEN_API_KEY);
+
+		SongParams p = new SongParams();
+		p.setTitle("Sugar");
+		p.setArtist("Maroon 5");
+		// p.setResults(100);
+		p.sortBy("song_hotttnesss", false);
+		List<Song> songs = en.searchSongs(p);
+
+		String lastTitle = "";
+		for (Song song : songs) {
+			if (!lastTitle.toLowerCase().equals(song.getTitle().toLowerCase())) {
+				StringBuilder sb = new StringBuilder();
+
+				System.out.println(song.getAnalysis().getTempo());
+				// sb.append(song.getTitle()).append(" by ").append(song.getArtistName()).append(" | ").append(song.getEnergy()).append(song.getTempo()).append(song.getTimeSignature()).append(song.getMode()).append(str)
+				// System.out.println( + " by " + song.getArtistName());
+				song.showAll();
+			}
+			lastTitle = song.getTitle();
+		}
+	}
+}
